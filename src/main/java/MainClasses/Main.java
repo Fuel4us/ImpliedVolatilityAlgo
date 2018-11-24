34401127a4803d218c8608fcdd4f3674d42b8cf8
Gonçalo Fonseca <goncalo7bfc@gmail.com>
2018-11-24 03:34:08 +0000

    Added stock yahoo API to test features. :rocket:

--- src/main/java/MainClasses/Main.java
+++ src/main/java/MainClasses/Main.java
@@ -1,17 +1,23 @@
 package MainClasses;
 
 import Algorithm.ImpliedVolatility;
+import Stocks.StockSearch;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         double valueToRisk = Double.parseDouble(JOptionPane.showInputDialog(null, "How much do you want to risk ?"));
         double iv = Double.parseDouble(JOptionPane.showInputDialog(null, "How much is the implied volatility ?"));
 
         ImpliedVolatility impliedVolatility = new ImpliedVolatility(valueToRisk, iv);
         impliedVolatility.operationCall();
+
+        /*StockSearch stockSearch = new StockSearch("ANET");
+        stockSearch.stockCaller();*/
+
         }
 }
